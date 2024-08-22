85ddc691ee0d4ed281d8a45b3bbb77538cba1b83
Truong Le <leminhtruong.vt@gmail.com>
2024-08-22 12:33:31 +0700

    add flutter-bloc & vim-fugitive

---
+++ .config/nvim/lua/martevol/plugins/lsp/flutter-bloc.lua
@@ -0,0 +1,17 @@
+return {
+  "wa11breaker/flutter-bloc.nvim",
+  config = function()
+    vim.keymap.set(
+      "n",
+      "<Leader>cfb",
+      "<cmd>lua require('flutter-bloc').create_bloc()<cr>",
+      { desc = "[C]reate [F]lutter [B]loc" }
+    )
+    vim.keymap.set(
+      "n",
+      "<Leader>cfc",
+      "<cmd>lua require('flutter-bloc').create_cubit()<cr>",
+      { desc = "[C]reate [F]lutter [C]ubit" }
+    )
+  end,
+}
